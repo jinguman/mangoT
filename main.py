0799471b121e355d0afc8275105e905d7d780584
jinguman <jinguman@gmail.com>
2015-10-22 14:16:33 +0900

    change generate trace 1->7

--- main.py
+++ main.py
@@ -28,19 +28,61 @@ def main():
 
     queue = Queue.Queue()
     gt = GenerateTrace(queue)
+    gt2 = GenerateTrace(queue)
+    gt3 = GenerateTrace(queue)
+    gt4 = GenerateTrace(queue)
+    gt5 = GenerateTrace(queue)
+    gt6 = GenerateTrace(queue)
+    gt7 = GenerateTrace(queue)
 
     #gt.set_buffer("test1",100)
 
     #station number
-    num_station = 100
+    num_station = 50
     nsamp = 100
 
-    network = "KG"
+    network = "KA"
     for i in range(num_station):
         station = "AAA" + str(i)
         gt.set_buffer(network,station, nsamp)
-
     gt.start()
+
+    network = "KB"
+    for i in range(num_station):
+        station = "BBB" + str(i)
+        gt2.set_buffer(network,station, nsamp)
+    gt2.start()
+
+    network = "KC"
+    for i in range(num_station):
+        station = "CCC" + str(i)
+        gt3.set_buffer(network,station, nsamp)
+    gt3.start()
+
+    network = "KD"
+    for i in range(num_station):
+        station = "DDD" + str(i)
+        gt4.set_buffer(network,station, nsamp)
+    gt4.start()
+
+    network = "KE"
+    for i in range(num_station):
+        station = "EEE" + str(i)
+        gt5.set_buffer(network,station, nsamp)
+    gt5.start()
+
+    network = "KF"
+    for i in range(num_station):
+        station = "FFF" + str(i)
+        gt6.set_buffer(network,station, nsamp)
+    gt6.start()
+
+    network = "KG"
+    for i in range(num_station):
+        station = "GGG" + str(i)
+        gt7.set_buffer(network,station, nsamp)
+    gt7.start()
+
 
     it = InsertTrace(mongo_name, db_name, queue)
     it.start()
